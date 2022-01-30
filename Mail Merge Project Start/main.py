6482a13443ce483f8d25d7b926c2f1ca4423cbcc
Pliszka <karoliniakkjk@gmail.com>
2022-01-30 17:56:02 +0100

    correct content in mails

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,8 +1,22 @@
-#TODO: Create a letter using starting_letter.txt 
+#TODO: Create a letter using starting_letter.txt
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('./Input/Letters/starting_letter.txt') as letter:
+    letter = letter.readlines()
+
+print(letter)
+with open('./Input/Names/invited_names.txt') as names:
+    names = names.readlines()
+just_names = []
+for name in names:
+    just_names.append(name.replace('\n', ""))
+
+to_change = letter[0]
+
+for name in just_names:
+    letter[0] = to_change.replace('[name]', f'{name}')
+    print(letter)
+    # with open(f'.\Output\ReadyToSend\InvitationFor{name}.txt', 'w') as invitation:
+    #     invitation.write(str(new_letter))
